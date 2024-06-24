09710f8c044b931f08036ac43dbc905a6c999cdd
veerukatiyar2017 <katiyarveeru@gmail.com>
2024-06-24 18:50:43 +0530

    new Initial commit

--- Sources/BGUtility/Array+Extensions.swift
+++ Sources/BGUtility/Array+Extensions.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+public extension Array {
+    subscript(safeIndex index: Int) -> Iterator.Element? {
+        return index >= 0 && index < endIndex ?
+        self[index] : nil
+    }
+}
--- Tests/BGUtilityTests/Array+ExtensionsTests.swift
+++ Tests/BGUtilityTests/Array+ExtensionsTests.swift
@@ -6,30 +6,20 @@
 //
 
 import XCTest
+@testable import BGUtility
 
 final class Array_ExtensionsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testProperValue() throws {
+       let numbers = [1,2,3,4,5]
+        let num1 = numbers[safeIndex: 0]
+        XCTAssert(num1 == 1)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testOutOfIndexValue() throws {
+       let numbers = [1,2,3,4,5]
+        let num1 = numbers[safeIndex: 10]
+        XCTAssert(num1 == nil)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
