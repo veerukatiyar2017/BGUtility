7d7fdf9d99f3e690b23bb91c79b7016b6add77d5
veerukatiyar2017 <katiyarveeru@gmail.com>
2024-06-24 18:12:02 +0530

    Initial Commit

---
+++ Tests/BGUtilityTests/BGUtilityTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import BGUtility
+
+final class BGUtilityTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
